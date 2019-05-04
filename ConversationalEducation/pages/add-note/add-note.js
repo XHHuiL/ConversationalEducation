dd451a1b8f9bbb6deee2f7670cf19e3ca55c1e6f
weirulei <16302010018@fudan.edu.cn>
2019-05-04 16:38:21 +0800

    完成新建笔记功能.

---
+++ ConversationalEducation/pages/add-note/add-note.js
@@ -0,0 +1,18 @@
+Page({
+
+  data: {
+
+  },
+
+  onLoad: function (options) {
+    this.setData({
+      content: options.content,
+    });
+  },
+
+  changeNote: function (e) {
+  },
+
+  saveNote: function () {
+  }
+})
